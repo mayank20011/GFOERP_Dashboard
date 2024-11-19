141e7fcc2f5cb188b86fcaa795620620adc7d53b
mayank20011 <mayank.behl.mb1@gmail.com>
2024-11-19 13:18:10 +0530

    Connecting with backend

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -24,7 +24,7 @@ let response;
     .then((response)=>
       {
         setLoading(false);
-        console.log(response.data.data);
+        setData(response.data.data);
       })
     .catch((err)=>{
       console.error("Error fetching data:", err);
