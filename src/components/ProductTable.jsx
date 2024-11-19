89fee73d0c7c5a4c21f5305c4c3e0300cb869196
mayank20011 <mayank.behl.mb1@gmail.com>
2024-11-19 12:57:27 +0530

    Tring to debug api problem

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,47 +1,3 @@
-// import React from "react";
-// import dataArray from "./Productrawtabledata.js";
-
-// const tableHeading = [
-//   "Client Name",
-//   "Quality",
-//   "Fat",
-//   "CLR",
-//   "Alchol",
-//   "Acidity",
-//   "Adulteration",
-//   "TimeStamp",
-//   "What To Do",
-// ];
-
-// function Table() {
-//   return (
-//     <table className="table-fixed">
-//       <thead>
-//         <tr>
-//           {tableHeading.map((data) => (
-//             <th className="p-4 border border-black" key={data}>{data}</th>
-//           ))}
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {dataArray.map((row,index) => {
-//           const objKeys = Object.keys(row);
-//           return(
-//           <tr key={index} className={ index%2==0? 'text-center bg-slate-200':'text-center'}>
-//             {objKeys.map((key)=>(
-//                 <td key={key} className='p-4 border border-black'>{row[key]}</td>
-//             ))}
-//           </tr>
-//           );
-//         })}
-//       </tbody>
-//     </table>
-//   );
-// }
-
-// export default Table;
-
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -58,26 +14,22 @@ const tableHeading = [
 ];
 
 function Table() {
-  const [data, setData] = useState([]);
+  const [dataa, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+
 let response;
 
-  useEffect(() => {
-    // Function to fetch data from API
-    const fetchData = async () => {
-      try {
-         response = await axios.get("https://gfoerp-mern-api.vercel.app/Purchase/");
+  useEffect(()=>{
+    axios.get("https://gfoerp-mern-api.vercel.app/Purchase/")
+    .then((response)=>
+      {
         setData(response.data);
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
         setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+      })
+    .catch((err)=>{
+      console.error("Error fetching data:", err);
+    });
+  },[]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -95,7 +47,7 @@ let response;
         </tr>
       </thead>
       <tbody>
-        {data.map((row, index) => {
+        {dataa.map((row, index) => {
           const objKeys = Object.keys(row);
           return (
             <tr key={index} className={index % 2 === 0 ? 'text-center bg-slate-200' : 'text-center'}>
